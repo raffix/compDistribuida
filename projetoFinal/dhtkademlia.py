ac29d1c4ebecf1fb17ba1673c77b658c8b9f80a8
raffixrcr <raffixrcr@hotmail.com>
2017-07-26 19:56:00 -0300

    Criado método para selecionar o host mais parecido com arquivo. - pf

--- projetoFinal/dhtkademlia.py
+++ projetoFinal/dhtkademlia.py
@@ -6,7 +6,8 @@ class dhtkad:
         self.myHash = hashlib.sha256()
         ident_bytes = ident.encode('utf-8')
         self.myHash.update(ident_bytes)
-        self.dht = {}
+        self.peers = {}
+        self.files = {}
 
     def compare(self, other):
         numberEquals = 0;
@@ -16,20 +17,42 @@ class dhtkad:
                 numberEquals=+1
         return numberEquals
 
+    def compareTwo(first, second):
+        numberEquals = 0;
+        for (n,m) in zip(first, second):
+            if (n == m) :
+                numberEquals=+1
+        return numberEquals
+
     def addHost(self, host):
         hostHash = hashlib.sha256()
         host_bytes = host.encode('utf-8')
         hostHash.update(host_bytes)
-#        if self.compare(hostHash.hexdigest()) > 1 :
-        if(not hostHash.hexdigest() in self.dht):
-            self.dht[hostHash.hexdigest()] = host
+        if(not hostHash.hexdigest() in self.peers):
+            self.peers[hostHash.hexdigest()] = host
         return 1
         
     def addFile(self, fileName):
         fileHash = hashlib.sha256()
         fileHash.update(fileName.encode('utf-8'))
         if (self.compare(fileHash.hexdigest()) is 1):
-            if(not fileHash.hexdigest() in self.dht):
-                self.dht[fileHash.hexdigest()] = fileName
+            if(not fileHash.hexdigest() in self.files):
+                self.files[fileHash.hexdigest()] = fileName
             return 1
         return 0
+        
+    #search for the host
+    def mostLikely(self, f):
+        keys = list(self.peers.keys())
+        like = ''
+        value = 0
+        fileHash = hashlib.sha256()
+        fileHash.update(f.encode('utf-8'))
+        for n in keys:
+            temp = self.compareTwo(fileHash.hexdigest(), n)
+            if (temp > value ):
+                value = temp
+                like = n
+        return like
+        
+        
